6e65c19902654ff904d9d81856082008c89a7bb9
Madhu Lokesh <madhumj623@gmail.com>
2023-11-13 23:36:50 +0530

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home';
 import { useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 import { useEffect } from 'react';
@@ -27,6 +28,7 @@ function App() {
       isTopOfPage = {isTopOfPage}
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}/>
+     <Home setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
