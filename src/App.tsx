a85aa98123c450624d71a6a95bcff86ccb57f662
Madhu Lokesh <madhumj623@gmail.com>
2023-11-14 16:37:59 +0530

    BenefitsPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
+import Benefits from './scenes/benefits/index';
 import { useState } from 'react';
 import { SelectedPage } from '@/shared/types';
 import { useEffect } from 'react';
@@ -29,6 +30,7 @@ function App() {
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}/>
      <Home setSelectedPage={setSelectedPage}/>
+     <Benefits setSelectedPage={setSelectedPage} />
     </div>
   )
 }
